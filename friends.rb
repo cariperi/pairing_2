a14b5ff03470f20fc39c3df39458df2ed1350ca2
Caroline Peri <caroline.anne.peri@gmail.com>
2022-12-07 20:45:50 -0500

    Add new friends

--- friends.rb
+++ friends.rb
@@ -1,6 +1,11 @@
-friend = 'Pegah'
+friends = ['Pegah', 'Johnny', 'Rando']
+friend = friends.sample
+puts "My friend is named #{friend}."
+
 if friend == 'Pegah'
     puts 'You are the best!'
+elsif friend == 'Johnny'
+    puts 'You are the man, bro.'
 else
-    puts 'You are pretty cool, friend.'
+    puts 'You are pretty cool, Rando.'
 end
